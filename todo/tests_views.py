0fb04e354030d812ecf6f45b810363b4b93c3d1a
Natalitta <>
2023-07-24 19:15:14 +0000

    Add testing models and can edit

--- todo/tests_views.py
+++ todo/tests_views.py
@@ -17,7 +17,7 @@ class TestViews(TestCase):
 
     def test_get_edit_item_page(self):
         item = Item.objects.create(name='Test Todo Item')
-        response = self.client.get(f'/edit/{item.id}]')
+        response = self.client.get(f'/edit/{item.id}')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/edit_item.html')
 
@@ -25,16 +25,23 @@ class TestViews(TestCase):
         response = self.client.post('/add', {'name': 'Test Added Item'})
         self.assertRedirects(response, '/')
 
-    def test_can_toggle_item(self):
+    def test_can_delete_item(self):
         item = Item.objects.create(name='Test Todo Item')
-        response = self.client.get(f'/delete/{item.id}]')
+        response = self.client.get(f'/delete/{item.id}')
         self.assertRedirects(response, '/')
         existing_items = Item.objects.filter(id=item.id)
         self.assertEqual(len(existing_items), 0)
 
-    def test_can_delete_item(self):
+    def test_can_toggle_item(self):
         item = Item.objects.create(name='Test Todo Item', done=True)
-        response = self.client.get(f'/toggle/{item.id}]')
+        response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
